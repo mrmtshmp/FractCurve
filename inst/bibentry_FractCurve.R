ddcfb6d03c8bfb38be232f8decff1466bdebe393
mrmtshmp <mr.mt.shm@gmail.com>
2019-10-27 22:00:40 +0900

    first commit

---
+++ inst/bibentry_FractCurve.R
@@ -0,0 +1,6 @@
+bibentry(bibtype = "Manual",
+         title = '{{FractCurve}: Finding a point of fracture on a curve.}',
+         author = c(person("Shimpei", "Morimoto")),
+         year = {2019},
+         url = {"https://github.com/mrmtshmp/FractCurve"}
+         )
